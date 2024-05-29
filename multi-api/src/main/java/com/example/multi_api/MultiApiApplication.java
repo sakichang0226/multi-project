f83a462ef158159ee2230b232bd211d1c7464f54
sakichang0226 <saki.develop0226@gmail.com>
2024-05-30 01:03:27 +0900

    feat:demo-apiからdatabaseのプロジェクト経由でデータ取得する

--- multi-api/src/main/java/com/example/multi_api/MultiApiApplication.java
+++ multi-api/src/main/java/com/example/multi_api/MultiApiApplication.java
@@ -2,8 +2,12 @@ package com.example.multi_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.example.multi_api",
+		"com.demo.database"
+})
 public class MultiApiApplication {
 
 	public static void main(String[] args) {
--- multi-api/src/main/java/com/example/multi_api/controller/DemoController.java
+++ multi-api/src/main/java/com/example/multi_api/controller/DemoController.java
@@ -1,16 +1,28 @@
 package com.example.multi_api.controller;
 
+import com.demo.database.model.Demo;
+import com.demo.database.service.DemoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
+@RequiredArgsConstructor
 public class DemoController {
 
-    @GetMapping("/demo")
-    public ResponseEntity<String> getDemo() {
-        return new ResponseEntity<String>("test", HttpStatus.OK);
+    private final DemoService service;
+
+    @GetMapping("/demo/{id}")
+    public ResponseEntity<Demo> getDemo(@PathVariable("id") Long id) {
+
+        Optional<Demo> demo = service.get(id);
+
+        return new ResponseEntity<>(demo.get(), HttpStatus.OK);
     }
 
 }
