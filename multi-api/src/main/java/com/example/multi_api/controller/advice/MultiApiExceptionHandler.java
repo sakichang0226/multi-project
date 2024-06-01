64f85ef688f4e9120672957f78d417f244f0ff50
sakichang0226 <saki.develop0226@gmail.com>
2024-06-01 20:57:09 +0900

    feat: プロジェクト用の例外の用意とエラーハンドリングを実施

--- multi-api/src/main/java/com/example/multi_api/controller/advice/MultiApiExceptionHandler.java
+++ multi-api/src/main/java/com/example/multi_api/controller/advice/MultiApiExceptionHandler.java
@@ -1,18 +1,21 @@
 package com.example.multi_api.controller.advice;
 
+import com.demo.demo_s3.exception.DemoS3BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @ControllerAdvice
 @Slf4j
-public class MultiApiExceptionHandler {
+public class MultiApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     /**
      * IOException発生時のエラーレスポンス
@@ -21,6 +24,21 @@ public class MultiApiExceptionHandler {
      */
     @ExceptionHandler(IOException.class)
     public ResponseEntity<Object> exceptionHandler(Exception exception){
+        log.warn(exception.getMessage());
+
+        Map<String, String> errorInfo = new HashMap<>();
+        errorInfo.put("code", "API_ERR_001");
+
+        return new ResponseEntity<>(errorInfo, HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * DemoS3BusinessException発生時のエラーレスポンス
+     * @param exception 例外クラス
+     * @return エラーレスポンス
+     */
+    @ExceptionHandler(DemoS3BusinessException.class)
+    public ResponseEntity<Object> handleException(DemoS3BusinessException exception){
         log.warn(exception.getMessage());
 
         Map<String, String> errorInfo = new HashMap<>();
