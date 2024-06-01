681605c182d212c5ff31a231471f5d62379e82de
sakichang0226 <saki.develop0226@gmail.com>
2024-06-01 15:47:35 +0900

    feat: エンドポイントを定数で管理する

---
+++ multi-api/src/main/java/com/example/multi_api/constant/ApiEndPoint.java
@@ -0,0 +1,11 @@
+package com.example.multi_api.constant;
+
+/**
+ * multi-apiのController層で設定するエンドポイントを管理する定数クラス
+ */
+public class ApiEndPoint {
+
+    /** s3Controllerの基底url　*/
+    public static final String S3_URL = "/s3";
+
+}
--- multi-api/src/main/java/com/example/multi_api/controller/S3Controller.java
+++ multi-api/src/main/java/com/example/multi_api/controller/S3Controller.java
@@ -7,31 +7,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Map;
 
+import static com.example.multi_api.constant.ApiEndPoint.S3_URL;
+
 @Controller
 @RequiredArgsConstructor
+@RequestMapping(value = S3_URL)
 public class S3Controller {
 
     private final S3Service s3Service;
 
-    @GetMapping("/s3")
+    @GetMapping("")
     public ResponseEntity<Map<String, String>> getYamlResponse() {
         Map<String, String> body = s3Service.getYamlFile();
 
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
-    @GetMapping("/s3/json")
+    @GetMapping("/json")
     public ResponseEntity<Test> getTestResponse() {
         Test body = s3Service.getTest();
 
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
-    @GetMapping("/s3/json/all")
+    @GetMapping("/json/all")
     public ResponseEntity<List<Test>> getTests() {
         List<Test> body = s3Service.getTestList();
 
