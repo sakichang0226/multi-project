29fb26b7ed0fe7060e4a450ce5bf5a466db79f9e
sakichang0226 <saki.develop0226@gmail.com>
2024-06-01 13:30:26 +0900

    fix: 不要なimport,throwsを削除

--- multi-api/src/main/java/com/example/multi_api/controller/S3Controller.java
+++ multi-api/src/main/java/com/example/multi_api/controller/S3Controller.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -20,7 +17,7 @@ public class S3Controller {
     private final S3Service s3Service;
 
     @GetMapping("/s3")
-    public ResponseEntity<Map<String, String>> getYamlResponse() throws IOException {
+    public ResponseEntity<Map<String, String>> getYamlResponse() {
         Map<String, String> body = s3Service.getYamlFile();
 
         return new ResponseEntity<>(body, HttpStatus.OK);
